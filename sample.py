7d12c9cc920a64730cf17a16398bcbbe10ebf9da
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-12-11 10:11:02 +1100

    python

---
+++ sample.py
@@ -0,0 +1,36 @@
+""" print  ("hello world ")
+print (10+10)
+a = 10+10
+print (a)
+print (type(a))
+print ("Result: ", a ) """
+
+# txt = "Hello WORLD "
+
+# x = txt.casefold()
+# print(x)
+
+# x = txt.upper()
+# print(x)
+
+# x = txt.index('e')
+# print(x)
+
+
+# x = txt.split()
+# print(x)
+
+# a = True
+# b = False
+# print (a and b)
+# print(a or b)
+
+a = 2 
+b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+# print(a//b)  #2 divided by 5 is 0.666.., but the // operator gives the integer part of the division, which is 0.     a=4 b= 2 answer 2
+# print(a ** b)  # a power b 2*2*2*2*2*
+print(dir())
